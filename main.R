2ae9ba1cefb0fda0205fdfea81535fb8c88e8dd8
Rick MarSal <richims026@gmail.com>
2024-07-08 09:12:42 -0500

    positionNudge

--- main.R
+++ main.R
@@ -1,10 +1,29 @@
 library(tidyverse)
 library(ggdark)
+library(magrittr)
 
-p<-
-mpg%>%
-ggplot(aes(displ,hwy))+
-geom_jitter()+
-geom_smooth()+
-dark_mode()
+p <-
+  mpg %>%
+  ggplot(aes(displ, hwy)) +
+  geom_jitter() +
+  geom_smooth() +
+  dark_mode()
 print(p)
+
+mpg %>% str()
+mpg$year %<>% as.factor()
+
+position_nudge(x = -.2, y = 0)
+
+mpg %>%
+  ggplot(aes(year, displ, col = year)) +
+  geom_jitter(width = .05, alpha = .2) +
+  geom_boxplot(
+    width = .1, alpha = .2,
+    position = position_nudge(x = -.2, y = 0)
+  ) +
+  stat_summary(
+    geom = "errorbar", fun.data = mean_cl_normal,
+    width = .05, position = position_nudge(x = .2, y = 0)
+  ) +
+  dark_mode() 
